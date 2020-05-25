b75dbccbfaf8cd44b50ee05073cd462bc15f0568
Einar Arnason <einsiarna@gmail.com>
2020-05-25 17:11:51 +0000

    Added DateVersioning module

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-import time
-import subprocess
+import DateVersioning
 from setuptools import setup
 
 with open("README.md") as f:
@@ -7,16 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="PyDigiPio",
-    version=time.strftime(
-        "%y.%j.%k%M%S",
-        time.localtime(
-            int(
-                subprocess.check_output("git show -s --format='%ct'", shell=True)
-                .decode()
-                .strip()
-            )
-        ),
-    ),
+    version=DateVersioning.generate(),
     description="Python module for Raspberry Pi GPIO",
     long_description_content_type="text/markdown",
     long_description=readme,
